6defac915836c04593f838fe2bb82b84f0fd105e
joseantonio <joseantoniomartinez1904@gmail.com>
2021-11-29 13:37:54 +0100

    first commit

---
+++ src/main/java/com/everis/springConfig/SpringConfigProject1Application.java
@@ -0,0 +1,13 @@
+package com.everis.springConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigProject1Application.class, args);
+	}
+
+}
